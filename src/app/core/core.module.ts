7635db1500498ec9b824e0693bd22167a8c70c83
Angel <zangelweb@gmail.com>
2021-08-18 16:41:29 -0500

    providers

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,14 +1,18 @@
+import { EpisodesService } from './services/episodes.service';
+import { CharactersService } from 'src/app/core/services/characters.service';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-
-
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     HttpClientModule
+  ],
+  providers: [
+    CharactersService,
+    EpisodesService
   ]
 })
 export class CoreModule { }
