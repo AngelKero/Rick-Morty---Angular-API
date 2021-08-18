bb8875352787e966ed7d8c89ddc531831d701c54
Angel <zangelweb@gmail.com>
2021-08-18 13:32:30 -0500

    nav mobile

--- src/app/shared/components/navigation/navigation.component.ts
+++ src/app/shared/components/navigation/navigation.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+  opened: boolean;
+
+  constructor() {
+    this.opened = false;
+  }
 
   ngOnInit(): void {
   }
